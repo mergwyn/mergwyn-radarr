57d751fc68f304450354f2d6d1e8b978f8192914
mergwyn <gary@theclarkhome.com>
2020-11-30 17:27:04 +0000

    Trying out inline template for version

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,16 +1,27 @@
 #
 class radarr::install {
+# Get the latest tag (replacves the custom fact"
+  $curl            = '/opt/puppetlabs/puppet/bin/curl'
+  $api_url         = 'https://api.github.com/repos/Radarr/Radarr/tags'
+  $jq_cmd          = 'jq --raw-output \'.[0].name\''
+  $new_version = inline_template("<%= `${curl} -s ${api_url} | ${jq_cmd}'` %>")
+  notify{"new version is ${new_version}":}
+  unless $new_verison =~ String[1] {
+    fail ("radarr version is '${new_version}'")
+  }
+
   unless $facts[radarr_version] =~ String[1] {
     fail ("radarr version is '${facts[radarr_version]}'")
   }
   $package_name    = 'Radarr.master'
   $package_version = $facts[radarr_version]
+  notify{ "package_version is ${package_version}":}
   $install_path    = $::radarr::install_path
   $extract_dir     = "${install_path}/Radarr-${package_version}"
   $creates         = "${extract_dir}/Radarr"
   $link            = "${install_path}/Radarr"
   $repository_url  = 'https://github.com/Radarr/Radarr/releases/download/'
-  $package_source  = "${repository_url}/${package_version}/${package_name}.${package_version[1,-1]}.linux-core-x64.tar.gz"
+  $package_source  = "${repository_url}/${package_version}/${package_name}.${package_version[1,-1]}.linux.tar.gz"
   $archive_name    = "${package_name}-${package_version}.linux.tar.gz"
   $archive_path    = "${install_path}/${archive_name}"
 
