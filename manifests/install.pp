c3058a82f25132dd9d8c9512e3a48961c42748c6
mergwyn <gary@theclarkhome.com>
2020-12-01 20:11:10 +0000

    Move to using api for all latest download link

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,22 +1,44 @@
 #
 class radarr::install {
+
+
 # Get the latest tag (replacves the custom fact"
-  $curl            = '/opt/puppetlabs/puppet/bin/curl'
-  $api_url         = 'https://api.github.com/repos/Radarr/Radarr/tags'
-  $jq_cmd          = 'jq --raw-output \'.[0].name\''
-  $package_version     = chomp(inline_template("<%= `${curl} -s ${api_url} | ${jq_cmd}` %>"))
+  $bindir = '/opt/puppetlabs/puppet/bin'
+  $curl   = "${bindir}/curl"
+  $jq     = '/usr/bin/jq'
+
+  $tags_url        = 'https://api.github.com/repos/Radarr/Radarr/tags'
+  $jq_version      = "${jq} --raw-output '.[0].name'"
+  $package_version = chomp(inline_template("<%= `${curl} -s ${tags_url} | ${jq_version}` %>"))
   unless $package_version =~ String[1] {
     fail ("radarr version is '${package_version}'")
   }
 
-  $package_name    = 'Radarr.master'
+  case $facts['os']['architecture'] {
+    'amd64': {
+      $arch = 'linux.core.x64'
+    }
+    default: {
+    fail("Architecture ${facts['os']['architecture']} is not supported")
+    }
+  }
+
+# Select the download url of the package that matches the arch above
+  $latest_url      = 'https://api.github.com/repos/Radarr/Radarr/releases/latest'
+  $jq_url          = "${jq} --raw-output '.assets[].browser_download_url'"
+  $package_source  = chomp(inline_template("<%= `${curl} -s ${latest_url} | ${jq_url} | grep ${arch}` %>"))
+  unless $package_source =~ String[1] {
+    fail ("Unable to get download url for Radarr version '${package_version}'")
+  }
+
+# get the archive name and strin gthe tar.gz suffix
+  $archive_name    = basename($package_source)
+  $package_name    = basename($archive_name,'.tar.gz')
+
   $install_path    = $::radarr::install_path
-  $extract_dir     = "${install_path}/Radarr-${package_version}"
+  $extract_dir     = "${install_path}/${package_name}"
   $creates         = "${extract_dir}/Radarr"
   $link            = "${install_path}/Radarr"
-  $repository_url  = 'https://github.com/Radarr/Radarr/releases/download/'
-  $package_source  = "${repository_url}/${package_version}/${package_name}.${package_version[1,-1]}.linux.tar.gz"
-  $archive_name    = "${package_name}-${package_version}.linux.tar.gz"
   $archive_path    = "${install_path}/${archive_name}"
 
   if $radarr::package_manage {
@@ -47,8 +69,7 @@ class radarr::install {
     exec {'radarr_tidy':
       cwd         => $install_path,
       path        => '/usr/sbin:/usr/bin:/sbin:/bin:',
-      command     => "ls -dtr ${link}-* | head -n -${radarr::keep} | xargs rm -rf",
-      #onlyif      => "test $(ls -d ${link}-* | wc -l) -gt ${radarr::keep}",
+      command     => "ls -dtr ${link}[.-]* | head -n -${radarr::keep} | xargs rm -rf",
       refreshonly => true,
       subscribe   => Archive[$archive_name],
     }
