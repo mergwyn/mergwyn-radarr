1c74834eea5623a6aebcfa677388981e8cc963a3
mergwyn <gary@theclarkhome.com>
2021-09-17 14:07:15 +0100

    Rewrite install as github_releases never refreshed archive

---
+++ lib/facter/radarr_version.rb
@@ -0,0 +1,20 @@
+require 'facter'
+
+# Default for non-Linux nodes
+#
+Facter.add(:radarr_version) do
+  setcode do
+    nil
+  end
+end
+
+# Linux
+#
+Facter.add(:radarr_version) do
+  confine kernel: :linux
+  setcode do
+    if system('which jq > /dev/null 2>&1') && system('which curl > /dev/null 2>&1')
+      Facter::Util::Resolution.exec('curl -s https://api.github.com/repos/Radarr/Radarr/tags | jq --raw-output ".[0].name"')
+    end
+  end
+end
--- manifests/install.pp
+++ manifests/install.pp
@@ -1,41 +1,74 @@
-# @summary Download github release and extract to install directory
-#
+# @summary install package from github
 class radarr::install {
 
   if $radarr::package_manage {
-    case $facts['os']['architecture'] {
-      'amd64': { $edition = 'linux-core-x64' }
-      default: { fail("Architecture ${facts['os']['architecture']} is not supported") }
-    }
 
-    $archive_name = "/Radarr.latest.${edition}.tar.gz"
-    $archive_path = "${facts['puppet_vardir']}/${archive_name}"
-    $install_path = $::radarr::install_path
-    $creates      = "${install_path}/Radarr"
+    if ! defined( Package[curl] ) { package { 'curl': ensure=>installed; } }
+    if ! defined( Package[jq] )   { package { 'jq':   ensure=>installed; } }
 
-    file { $install_path :
-      ensure => directory,
-      owner  => $::radarr::user,
-      group  => $::radarr::group,
-    }
+# Only execute if radarr version has already been defined (first run may need the
+# packages above to be installed
+
+    if $::radarr_version {
+      # Make sure version has is a string with at least 1 char
+      unless $::radarr_version =~ String[1] {
+        fail ("radarr_version cannot be an empty string '${::radarr_version}'")
+      }
+      case $facts['os']['architecture'] {
+        'amd64': { $edition = 'linux-core-x64' }
+        default: { fail("Architecture ${facts['os']['architecture']} is not supported") }
+      }
+# Radarr.master.3.2.2.5080.linux-core-arm64.tar.gz
+# https://github.com/Radarr/Radarr/releases/download/v3.2.2.5080/Radarr.master.3.2.2.5080.linux-core-arm64.tar.gz
 
 
-    githubreleases_download { $archive_path:
-      author            => 'Radarr',
-      repository        => 'Radarr',
-      asset             => true,
-      asset_filepattern => $edition,
-    }
-    -> archive { $archive_name:
-      source       => "file://${archive_path}",
-      extract      => true,
-      extract_path => $install_path,
-      creates      => $creates,
-      cleanup      => false,
-      user         => $::radarr::user,
-      group        => $::radarr::group,
-      notify       => Service['radarr.service'],
-      require      => Githubreleases_download[$archive_path],
+      $short_version   = $::radarr_version[1,-1]
+      $package_version = $::radarr_version
+      $install_path    = $::radarr::install_path
+      $package_name    = 'Radarr.master'
+      $extract_dir     = "${install_path}/Radarr-${package_version}"
+      $creates         = "${extract_dir}/Radarr"
+      $link            = "${install_path}/Radarr"
+      $repository_url  = 'https://github.com/Radarr/Radarr/releases/download'
+      $archive_name    = "${package_name}.${short_version}.${edition}.tar.gz"
+      $package_source  = "${repository_url}/${package_version}/${archive_name}"
+      $archive_path    = "${install_path}/${archive_name}"
+
+      if $radarr::package_manage {
+
+        file { $extract_dir:
+          ensure => directory,
+          owner  => $::radarr::user,
+          group  => $::radarr::group,
+        }
+
+        archive { $archive_name:
+          path         => $archive_path,
+          source       => $package_source,
+          extract      => true,
+          extract_path => $extract_dir,
+          creates      => $creates,
+          cleanup      => true,
+          user         => $::radarr::user,
+          group        => $::radarr::group,
+          #TODO reference to mono
+          #require      => Class['mono'],
+          notify       => Service['radarr.service'],
+        }
+        file { $link:
+          ensure    => 'link',
+          target    => $creates,
+          subscribe => Archive[$archive_name],
+        }
+        exec {'radarr_tidy':
+          cwd         => $install_path,
+          path        => '/usr/sbin:/usr/bin:/sbin:/bin:',
+          command     => "ls -dtr ${link}-* | head -n -${radarr::keep} | xargs rm -rf",
+          #onlyif      => "test $(ls -d ${link}-* | wc -l) -gt ${radarr::keep}",
+          refreshonly => true,
+          subscribe   => Archive[$archive_name],
+        }
+      }
     }
   }
 }
